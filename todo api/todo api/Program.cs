11d3626f5d024ebb40eb25c00e8e9c62082fd439
niraj <nirajparadva2004@gmail.com>
2025-08-05 18:51:59 +0530

    Allows To Access frontend (CORS)

--- todo api/todo api/Program.cs
+++ todo api/todo api/Program.cs
@@ -13,6 +13,19 @@ builder.Services.AddSwaggerGen();
 //todo services aadded
 builder.Services.AddSingleton<TodoServices>();
 
+//cors add
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173") // React dev server
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+
 var app = builder.Build();
 
 app.MapControllers();
@@ -23,6 +36,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowReactApp");
 
 app.UseHttpsRedirection();
 
